27757f1a55cf6b66681cb5e6e76bacf237558bb2
An Trinh <atrinh@gmail.com>
2022-05-23 14:38:26 +0100

    Update scope of urlString to private Update fetchData function to use `badJSON` error

--- Meme Creator.swiftpm/App/PandaCollectionFetcher.swift
+++ Meme Creator.swiftpm/App/PandaCollectionFetcher.swift
@@ -1,6 +1,6 @@
 /*
-See the License.txt file for this sample’s licensing information.
-*/
+ See the License.txt file for this sample’s licensing information.
+ */
 
 import SwiftUI
 
@@ -9,22 +9,25 @@ class PandaCollectionFetcher: ObservableObject {
     @Published var imageData = PandaCollection(sample: [Panda.defaultPanda])
     @Published var currentPanda = Panda.defaultPanda
     
-    let urlString = "http://playgrounds-cdn.apple.com/assets/pandaData.json"
+    private let urlString = "http://playgrounds-cdn.apple.com/assets/pandaData.json"
     
     enum FetchError: Error {
         case badRequest
         case badJSON
     }
-    
-    @available(iOS 15.0, *)
-     func fetchData() async 
-     throws  {
+
+    func fetchData() async throws {
         guard let url = URL(string: urlString) else { return }
 
         let (data, response) = try await URLSession.shared.data(for: URLRequest(url: url))
-        guard (response as? HTTPURLResponse)?.statusCode == 200 else { throw FetchError.badRequest }
+        guard (response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw FetchError.badRequest
+        }
 
-        imageData = try JSONDecoder().decode(PandaCollection.self, from: data)
+        do {
+            imageData = try JSONDecoder().decode(PandaCollection.self, from: data)
+        } catch {
+            throw FetchError.badJSON
+        }
     }
-    
 }
