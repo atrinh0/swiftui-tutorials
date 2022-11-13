f27618c8dac1b4cddf570c8fa715a5155ebae4ee
An Trinh <atrinh@gmail.com>
2022-11-13 17:09:46 +0000

    Add alternate rotation effect by using rotation3DEffect instead of width changes

--- Animating Shapes.swiftpm/App/SpinningView.swift
+++ Animating Shapes.swiftpm/App/SpinningView.swift
@@ -1,6 +1,6 @@
 /*
-See the License.txt file for this sample’s licensing information.
-*/
+ See the License.txt file for this sample’s licensing information.
+ */
 
 import SwiftUI
 
@@ -11,37 +11,43 @@ struct ResetEllipse: View {
             .foregroundColor(.orange)
     }
 }
-struct SpinningEllipse: View {
-        @State private var width: CGFloat = 200
 
-        var body: some View {
-            Ellipse()
-                .frame(width: width, height: 200)
-                .foregroundColor(.orange)
-                .onAppear {
-                    withAnimation(.easeInOut.repeatForever(autoreverses: true).speed(2)) {
-                        width = 0
-                    }
+struct SpinningEllipse: View {
+    @State private var isAnimating = false
+
+    var body: some View {
+        Ellipse()
+            .frame(width: 200, height: 200)
+            .foregroundColor(.orange)
+            .overlay {
+                Image(systemName: "sterlingsign")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150, height: 150)
+                    .foregroundColor(.white)
+            }
+            .rotation3DEffect(.degrees(isAnimating ? 0 : 360), axis: (x: 0, y: 1, z: 0))
+            .onAppear {
+                withAnimation(.easeInOut.repeatForever(autoreverses: false).speed(0.25)) {
+                    isAnimating = true
                 }
-        }
+            }
+    }
 }
+
 struct SpinningView: View {
     @State private var isSpinning = false
     
     var body: some View {
         VStack {
-            
             Spacer()
-            
             if isSpinning {
                 SpinningEllipse()
             } else {
                 ResetEllipse()
             }
-            
             Spacer()
-            
-            PlayResetButton(animating: $isSpinning) 
+            PlayResetButton(animating: $isSpinning)
         }
         .navigationTitle("Spinning Animation")
         .navigationBarTitleDisplayMode(.inline)
