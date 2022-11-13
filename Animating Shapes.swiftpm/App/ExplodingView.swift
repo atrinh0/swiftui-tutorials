a8e62c12bc280ee4d22b5767bdee10681ff46ed9
An Trinh <atrinh@gmail.com>
2022-11-13 17:29:21 +0000

    Enhance exploding square animation

--- Animating Shapes.swiftpm/App/ExplodingView.swift
+++ Animating Shapes.swiftpm/App/ExplodingView.swift
@@ -11,28 +11,31 @@ struct ExplodingView: View {
         VStack {
             Spacer()
             ZStack {
-                ForEach(0..<75) { _ in
-                    Rectangle()
-                        .rotation(Angle(degrees: Double.random(in: 0..<360)))
-                        .frame(width: 20, height: 20)
-                        .foregroundColor(.indigo)
-                        .offset(x: isExploded ? (Double.random(in: -1...1) * 500) : 0, y: isExploded ? (Double.random(in: -1...1) * 500) : 0)
-                        .opacity(isExploded ? 0 : 1)
-                        .animation(.easeInOut.speed(0.6), value: isExploded)
-                        .padding()
+                ForEach(0..<15) { row in
+                    ForEach(0..<15) { col in
+                        Rectangle()
+                            .rotation(Angle(degrees: isExploded ? Double.random(in: 0..<360) : 0))
+                            .frame(width: 20, height: 20)
+                            .foregroundColor(.indigo)
+                            .scaleEffect(isExploded ? Double.random(in: 1...2) : 1)
+                            .offset(x: isExploded ? randomDistance : CGFloat(150 - row * 20),
+                                    y: isExploded ? randomDistance : CGFloat(150 - col * 20))
+                            .opacity(isExploded ? 0 : 1)
+                            .animation(isExploded ? .easeOut.speed(Double.random(in: 0.6...1)) : .spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0.3),
+                                       value: isExploded)
+                            .padding()
+                    }
                 }
-                Rectangle()
-                    .frame(width: 300, height: 300)
-                    .foregroundColor(.indigo)
-                    .opacity(isExploded ? 0 : 1)
-                    .animation(.easeInOut.speed(0.6), value: isExploded)
-                    .padding()
             }
             Spacer()
             PlayResetButton(animating: $isExploded)
         }
         .navigationTitle("Explosion Animation")
         .navigationBarTitleDisplayMode(.inline)
+    }
+
+    private var randomDistance: CGFloat {
+        (Double.random(in: -1...1) * 500)
     }
 }
 
