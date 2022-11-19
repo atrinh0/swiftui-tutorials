c1693557d4353690a955b8aa01a96c8dc82605af
An Trinh <atrinh@gmail.com>
2022-11-19 21:36:57 +0000

    Experiment with rolling rainbow effect

--- Animating Shapes.swiftpm/App/RollinRainbow.swift
+++ Animating Shapes.swiftpm/App/RollinRainbow.swift
@@ -1,23 +1,22 @@
 /*
-See the License.txt file for this sample’s licensing information.
-*/
+ See the License.txt file for this sample’s licensing information.
+ */
 
 import SwiftUI
 
 struct RollinRainbowView: View {
     @State private var isAnimating = false
-    @State private var gridColumns = Array(repeating: GridItem(.flexible()), count: 10)
-    @State private var colors: [Color] = [.pink, .mint, .orange, .teal, .yellow, .cyan, .purple, .blue]
-    @State private var scaleFactor : CGFloat = 3
+    @State private var gridColumns = Array(repeating: GridItem(.flexible()), count: 20)
+    @State private var colors: [Color] = [.red, .orange, .yellow, .green, .blue, .indigo, .purple]
+    @State private var scaleFactor: CGFloat = 3
     
-    let springAnimation = Animation.spring(response: 0.4, dampingFraction: 0.75, blendDuration: 0.9)
-    
-    let numCircles = 100
+    let springAnimation = Animation.spring(response: 0.3, dampingFraction: 0.3, blendDuration: 0.3)
+    let numCircles = 500
+
     var body: some View {
         VStack {
             Text("Tap on the dots to make them roll!")
                 .padding(.vertical, 30)
-            
             Spacer()
             LazyVGrid(columns: gridColumns) {
                 ForEach(0..<numCircles, id: \.self) { index in
@@ -25,18 +24,18 @@ struct RollinRainbowView: View {
                     Circle()
                         .foregroundColor(colors[index % colors.count])
                         .scaleEffect(scaleFactor)
-                        .animation(springAnimation.delay((Double(index).truncatingRemainder(dividingBy: 10) / 20)).repeatCount(2, autoreverses: true), value: scaleFactor)
+                        .animation(springAnimation.delay((Double(index).truncatingRemainder(dividingBy: Double(colors.count)) / 20)),
+                                   value: scaleFactor)
                         .onTapGesture {
                             isAnimating = true
                             scaleFactor = (Double(index + 1) / 4)
                         }
                 }
             }
-            
+            .padding()
             Spacer()
-            
             PlayResetButton(animating: $isAnimating, resetOnly: true) {
-                    scaleFactor = 3
+                scaleFactor = 3
             }
         }
         .navigationTitle("Rollin' Rainbow")
